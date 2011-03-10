3ab8792d4975e55ba2f1a05130e9525f126da070
Alan <anochens@andrew.cmu.edu>
2011-03-10 01:11:05 -0500

    Version bump to 0.6.1

---
+++ example.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'lib/libthetvdb.rb'
+
+#To set the apikey use:
+#Thetvdb.apikey="MY_API_KEY_HERE"
+Thetvdb.apikey=`cat ./apikey.txt`.chomp
+
+#get series info and all episode info
+#same result as infoForSeriesId() followed by getAllEpisodes()
+#use this instead, because it is more efficient
+pp Thetvdb.getFullSeriesRecord("75760")
+
+#get all episodes for a given series, by id
+pp Thetvdb.getAllEpisodes("75760")
+
+#get all of the series id's as an array
+pp Thetvdb.getAllSeriesIds()
+
+#search the db for shows by name, multiple things returned
+pp Thetvdb.search("scrubs")
+
+#gets just series information for a given show
+pp Thetvdb.infoForSeriesId("75760")
